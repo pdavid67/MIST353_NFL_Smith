3fa26339fb31a4c1b25fc23f3ec3cd0e43c4a061
pdavid67 <pds00013@mix.wvu.edu>
2026-02-26 19:13:40 -0500

    update 3

--- DATA/DatabaseProgrammingObjectsSmith.sql
+++ DATA/DatabaseProgrammingObjectsSmith.sql
@@ -1,2 +1,19 @@
 -- 3 query
 -- 1 for each ConfrenceDivision and team Tables, and 1 inner join query
+
+
+SELECT *
+FROM ConferenceDivision;
+
+
+SELECT *
+FROM Team;
+
+
+SELECT t.TeamID,
+	t.TeamName,
+	cd.ConferenceName,
+	cd.DivisionName
+FROM Team AS t
+INNER JOIN ConferenceDivision AS cd
+	ON t.DivisionID = cd.DivisionID;
