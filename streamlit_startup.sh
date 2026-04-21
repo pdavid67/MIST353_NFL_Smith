8f673974eb9ffc8978ed8775cf7e488a061def07
pdavid67 <pds00013@mix.wvu.edu>
2026-04-20 20:45:06 -0400

    Use visible Streamlit dependency package

--- streamlit_startup.sh
+++ streamlit_startup.sh
@@ -1,6 +1,6 @@
 set -e
 
-PACKAGE_DIR="/home/site/wwwroot/.python_packages/lib/site-packages"
+PACKAGE_DIR="/home/site/wwwroot/python_packages/lib/site-packages"
 
 export PYTHONPATH="$PACKAGE_DIR:$PYTHONPATH"
 python -m streamlit run UI/nfl_playoffs_ui.py --server.port 8000 --server.address 0.0.0.0 --server.headless true
