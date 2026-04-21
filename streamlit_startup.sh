e8a47d8596e3e6b744aba8f1a7fb53758d193005
pdavid67 <pds00013@mix.wvu.edu>
2026-04-20 20:28:44 -0400

    Package Streamlit dependencies for Azure

--- streamlit_startup.sh
+++ streamlit_startup.sh
@@ -2,9 +2,5 @@ set -e
 
 PACKAGE_DIR="/home/site/wwwroot/.python_packages/lib/site-packages"
 
-if [ ! -d "$PACKAGE_DIR/streamlit" ]; then
-  python -m pip install --target "$PACKAGE_DIR" -r requirements.txt
-fi
-
 export PYTHONPATH="$PACKAGE_DIR:$PYTHONPATH"
 python -m streamlit run UI/nfl_playoffs_ui.py --server.port 8000 --server.address 0.0.0.0 --server.headless true
